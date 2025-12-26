41c50ae4a5cd1a9fef3d08c8a06a3686cb5a7354
WilliamMonceaux <williammonceauxalt@gmail.com>
2025-12-26 17:57:58 -0600

    imported Shop page component and added Route to it

--- mart-wal-.com/src/App.jsx
+++ mart-wal-.com/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Home } from './pages/Home/Home.jsx';
+import { Shop } from './pages/Shop/Shop.jsx';
 
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/Shop' element={<Shop />} />
     </Routes>
   );
 }
