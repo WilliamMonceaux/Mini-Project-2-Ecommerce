c3143f8cb57d34e81a58283e29507e768edd79c0
WilliamMonceaux <williammonceauxalt@gmail.com>
2025-12-26 08:29:23 -0600

    added a Promotional component that will display items that the website offers

---
+++ mart-wal-.com/src/components/PromoContainer.jsx
@@ -0,0 +1,12 @@
+import './promo-container.css'
+
+function PromoContainer() {
+    return(
+        <div className='flex-container'>
+    <div className='promo-container'>
+<p> Hi </p>
+    </div>
+    </div>);
+}
+
+export { PromoContainer };
