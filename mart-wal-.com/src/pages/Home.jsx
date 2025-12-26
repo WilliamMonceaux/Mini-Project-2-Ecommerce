d2ea0af96d40ae483a02e13a91b494e92ed61b14
WilliamMonceaux <williammonceauxalt@gmail.com>
2025-12-26 08:10:55 -0600

    added pages for different routes in website

---
+++ mart-wal-.com/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { NavBar } from '../components/NavBar';
+import { Header } from '../components/Header';
+
+function Home() {
+    return(
+        <>
+        <Header />
+        <NavBar />
+        </>
+    );
+}
+
+export { Home };
