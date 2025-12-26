671b4db29536497e92d5e0d3c5d3d6ac9e03614a
WilliamMonceaux <williammonceauxalt@gmail.com>
2025-12-26 08:31:21 -0600

    imported and added PromoContainer component

--- mart-wal-.com/src/pages/Home.jsx
+++ mart-wal-.com/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import { NavBar } from '../components/NavBar';
 import { Header } from '../components/Header';
+import { PromoContainer } from '../components/PromoContainer';
 
 function Home() {
     return(
         <>
         <Header />
         <NavBar />
+        <PromoContainer />
         </>
     );
 }
