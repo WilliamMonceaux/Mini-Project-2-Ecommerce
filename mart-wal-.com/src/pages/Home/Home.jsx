808b2e9046d8f2150ff0aabf7d0e29f4774fd3e2
WilliamMonceaux <williammonceauxalt@gmail.com>
2025-12-26 17:48:22 -0600

    imported NavBar, Header, and PromoContainer components

--- mart-wal-.com/src/pages/Home/Home.jsx
+++ mart-wal-.com/src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
-import { NavBar } from '../components/NavBar';
-import { Header } from '../components/Header';
-import { PromoContainer } from '../components/PromoContainer';
-import '../components/home.css';
+import { NavBar } from '../../components/NavBar';
+import { Header } from '../../components/Header';
+import { PromoContainer } from '../../components/PromoContainer';
 
 function Home() {
     return(
