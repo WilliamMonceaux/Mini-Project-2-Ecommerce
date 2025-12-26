8c4f8b7a39f571108744c9c24c33ff31a5e2e51c
WilliamMonceaux <williammonceauxalt@gmail.com>
2025-12-26 07:08:08 -0600

    added a unordered list list item for each route

--- mart-wal-.com/src/components/NavBar.jsx
+++ mart-wal-.com/src/components/NavBar.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
+import './global.css';
 
  function NavBar() {
-    return(<nav>
-        <Link to ='/'>Home</Link>
-        <Link to ='/shop'>Shop</Link>
-        <Link to ='/cart'>Cart</Link>
-        <Link to ='/about'>About</Link>
+    return(<nav className='navbar component-box'>
+        <ul>
+            <li> <Link to ='/'>Home</Link></li>
+            <li><Link to ='/shop'>Shop</Link></li>
+            <li><Link to ='/cart'>Cart</Link></li>
+            <li><Link to ='/about'>About</Link></li>
+        </ul>
     </nav>);
 }
 
