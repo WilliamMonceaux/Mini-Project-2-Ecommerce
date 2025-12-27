2c4401b65543dd075462deec785924a94536a598
WilliamMonceaux <williammonceauxalt@gmail.com>
2025-12-27 09:57:12 -0600

    added About page component and its files

--- mart-wal-.com/src/pages/About/About.jsx
+++ mart-wal-.com/src/pages/About/About.jsx
@@ -1,11 +1,18 @@
 import { NavBar } from '../../components/NavBar/NavBar';
 import { Header } from '../../components/Header/Header';
+import './about.css';
 
 
 function About() {
     return(<>
     <Header />
     <NavBar />
+    <main>
+        <section>
+            <p>This project is to demonstrate my ability to apply using and creating React Components, using hooks, fetching and displaying data from external API, </p>
+            <p>integrating React UI Library (This case Material UI), and with Git commits and pushes.</p>
+        </section>
+    </main>
     </>);
 }
 
