da1b4eaa29ba9be27aa69a0855e20501ad2301a3
WilliamMonceaux <williammonceauxalt@gmail.com>
2025-12-27 07:54:38 -0600

    reorganized files to more easily separate components and read them

---
+++ mart-wal-.com/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+ function NavBar() {
+    return(<nav className='navbar component-box'>
+        <ul>
+            <li> <Link to ='/'>Home</Link></li>
+            <li><Link to ='/shop'>Shop</Link></li>
+            <li><Link to ='/cart'>Cart</Link></li>
+            <li><Link to ='/about'>About</Link></li>
+        </ul>
+    </nav>);
+ }
+
+export { NavBar };
---
+++ mart-wal-.com/src/components/PromoContainer/PromoContainer.jsx
@@ -0,0 +1,17 @@
+import "./promo-container.css";
+
+function PromoContainer() {
+  return (
+    <div className="flex-container">
+      <div className="promo-container">
+        <div className='promo-header'>
+        <strong>
+            Featured Products:
+        </strong>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { PromoContainer };
