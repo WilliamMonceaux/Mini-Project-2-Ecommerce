c8463dd20375b366d9717d5e515aa564fbd10d44
WilliamMonceaux <williammonceauxalt@gmail.com>
2025-12-27 09:24:01 -0600

    imported About page component and added route to it

--- mart-wal-.com/src/App.jsx
+++ mart-wal-.com/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Home } from './pages/Home/Home.jsx';
 import { Shop } from './pages/Shop/Shop.jsx';
 import { Cart } from './pages/Cart/Cart.jsx';
+import { About } from './pages/About/About.jsx';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path='/' element={<Home />} />
       <Route path='/Shop' element={<Shop />} />
       <Route path='/Cart' element={<Cart />} />
+      <Route path='/About' element={<About />} />
     </Routes>
   );
 }
