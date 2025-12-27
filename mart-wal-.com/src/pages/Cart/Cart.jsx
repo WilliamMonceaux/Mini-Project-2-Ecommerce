4987e362bfe422a2ca240fd3358ce018c9ee77ec
WilliamMonceaux <williammonceauxalt@gmail.com>
2025-12-27 09:14:43 -0600

    added Cart page component that is used for Navigating to products in cart

---
+++ mart-wal-.com/src/pages/Cart/Cart.jsx
@@ -0,0 +1,11 @@
+import { NavBar } from '../../components/NavBar/NavBar';
+import { Header } from '../../components/Header/Header';
+
+function Cart() {
+    return(<>
+    <Header />
+    <NavBar />
+    </>);
+}
+
+export { Cart };
