f9c0af90ea6c51e67dd675f870947651a17c9649
WilliamMonceaux <williammonceauxalt@gmail.com>
2025-12-27 09:07:39 -0600

    added Card prototype in display

--- mart-wal-.com/src/components/Cards/CardsTemplate.jsx
+++ mart-wal-.com/src/components/Cards/CardsTemplate.jsx
@@ -1,13 +1,29 @@
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
-import Typpography from '@mui/material/Typography';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
 function CardsTemplate() {
-  return(<>
-  </>);
+  return(<section className='card-container'>
+    <Card sx={{ maxWidth: 345 }}>
+    <CardMedia component='img' alt='product' height='140' />
+    <CardContent>
+        <Typography gutterBottom variant='h5' component='div'>
+            Products
+        </Typography>
+        <Typography variant='body2' sx={{ color: 'text.secondary' }}>
+            Here is the products
+        </Typography>
+    </CardContent>
+    <CardActions>
+        <Button size='small'>Add to Card</Button>
+    </CardActions>
+  </Card>
+  </section>
+);
 }
 
 export { CardsTemplate };
