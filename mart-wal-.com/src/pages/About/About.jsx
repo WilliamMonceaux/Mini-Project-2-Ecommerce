473668084eadfd34024d79adbf220a5925ce2a21
WilliamMonceaux <williammonceauxalt@gmail.com>
2025-12-27 09:23:39 -0600

    created About directory for About page

---
+++ mart-wal-.com/src/pages/About/About.jsx
@@ -0,0 +1,12 @@
+import { NavBar } from '../../components/NavBar/NavBar';
+import { Header } from '../../components/Header/Header';
+
+
+function About() {
+    return(<>
+    <Header />
+    <NavBar />
+    </>);
+}
+
+export { About };
