720b0ae14c456d5b7469b3f863e062b8e563f3f2
WilliamMonceaux <williammonceauxalt@gmail.com>
2025-12-30 07:46:57 -0600

    successfully fetched store api from node.js using axios

--- mart-wal-.com/src/pages/Shop/Shop.jsx
+++ mart-wal-.com/src/pages/Shop/Shop.jsx
@@ -1,6 +1,31 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Header } from '../../components/Header/Header.jsx';
+import { NavBar } from '../../components/NavBar/NavBar.jsx';
+import { CardsTemplate } from '../../components/Cards/CardsTemplate.jsx';
 
 function Shop() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+
+            try {
+                const response = await axios.get('/api/shop/products')
+                setProducts(response.data);
+                
+            } catch (error) {
+                console.error('Error: Could not fetch data.', error.message);
+            }
+        };
+        
+        fetchProducts();
+    }, [])
+
     return(<>
+    <Header />
+    <NavBar />
+    <CardsTemplate products={products} />
     </>);
 }
 
