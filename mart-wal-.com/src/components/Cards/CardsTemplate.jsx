96629a1606ec8c4f5aca5c6363d14832d0d2a9ec
WilliamMonceaux <williammonceauxalt@gmail.com>
2025-12-30 08:26:56 -0600

    sucessfully implemented a working, temporary template to render products

--- mart-wal-.com/src/components/Cards/CardsTemplate.jsx
+++ mart-wal-.com/src/components/Cards/CardsTemplate.jsx
@@ -1,29 +1,46 @@
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
-function CardsTemplate() {
-  return(<section className='card-container'>
-    <Card sx={{ maxWidth: 345 }}>
-    <CardMedia component='img' alt='product' height='140' />
-    <CardContent>
-        <Typography gutterBottom variant='h5' component='div'>
-            Products
-        </Typography>
-        <Typography variant='body2' sx={{ color: 'text.secondary' }}>
-            Here is the products
-        </Typography>
-    </CardContent>
-    <CardActions>
-        <Button size='small'>Add to Card</Button>
-    </CardActions>
-  </Card>
-  </section>
-);
+function CardsTemplate({ products }) {
+  return (
+    <main className="card-container">
+      <Grid container spacing={2}>
+        {products.map((product) => {
+          return (
+          <Grid item key={product.id} size={{ xs: 12, md: 6, lg: 3 }}>
+            <Card sx={{ maxWidth: 300 }}>
+              <CardMedia
+                component="img"
+                image={product.image}
+                alt={product.title}
+                height="300"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {product.title}
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  {product.description}
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.primary" }}>
+                  {product.price}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Add to Card</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          );
+        })}
+      </Grid>
+    </main>
+  );
 }
 
 export { CardsTemplate };
