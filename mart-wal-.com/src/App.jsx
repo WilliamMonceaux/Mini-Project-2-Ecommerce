35433cceea3d17fb1c19931b7fbd24fd7be88854
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-01 07:41:27 -0600

    imported Signup page component; routing new page

--- mart-wal-.com/src/App.jsx
+++ mart-wal-.com/src/App.jsx
@@ -3,6 +3,7 @@ import { Home } from './pages/Home/Home.jsx';
 import { Shop } from './pages/Shop/Shop.jsx';
 import { Cart } from './pages/Cart/Cart.jsx';
 import { About } from './pages/About/About.jsx';
+import { Signup } from './pages/Sign-up/Sign-up.jsx';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path='/' element={<Home />} />
       <Route path='/Shop' element={<Shop />} />
       <Route path='/Cart' element={<Cart />} />
+      <Route path='/Sign-up' element={<Signup />} />
       <Route path='/About' element={<About />} />
     </Routes>
   );
