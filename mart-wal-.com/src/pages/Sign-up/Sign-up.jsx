7b5fc71dec3fe119919e62ba5868d03a85a81a4a
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-01 07:42:03 -0600

    created signup page for user to create an account

---
+++ mart-wal-.com/src/pages/Sign-up/Sign-up.jsx
@@ -0,0 +1,12 @@
+import { NavBar } from '../../components/NavBar/NavBar';
+import { Header } from '../../components/Header/Header';  
+
+function Signup() {
+    return(
+    <> 
+    <Header />
+    <NavBar />
+    </>);
+}
+
+export { Signup };
