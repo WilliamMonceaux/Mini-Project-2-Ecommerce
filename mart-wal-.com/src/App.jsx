039f705bbc0b5e2f04335b3b78bc355f5b6b66b4
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-01 18:03:27 -0600

    updated name for signup file

--- mart-wal-.com/src/App.jsx
+++ mart-wal-.com/src/App.jsx
@@ -3,7 +3,7 @@ import { Home } from './pages/Home/Home.jsx';
 import { Shop } from './pages/Shop/Shop.jsx';
 import { Cart } from './pages/Cart/Cart.jsx';
 import { About } from './pages/About/About.jsx';
-import { Signup } from './pages/Sign-up/Sign-up.jsx';
+import { SignUp } from './pages/Sign-up/SignUp.jsx';
 
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <Route path='/' element={<Home />} />
       <Route path='/Shop' element={<Shop />} />
       <Route path='/Cart' element={<Cart />} />
-      <Route path='/Sign-up' element={<Signup />} />
+      <Route path='/Sign-up' element={<SignUp />} />
       <Route path='/About' element={<About />} />
     </Routes>
   );
