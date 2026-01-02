439bfb12a46aa91003e8f6b26acdd9681a36d32d
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-01 18:03:46 -0600

    updated file name

---
+++ mart-wal-.com/src/pages/Sign-up/SignUp.jsx
@@ -0,0 +1,11 @@
+import { NavBar } from '../../components/NavBar/NavBar';
+import { Header } from '../../components/Header/Header';
+
+function SignUp() {
+    return(<>
+    <Header />
+    <NavBar />
+    </>);
+}
+
+export { SignUp };
