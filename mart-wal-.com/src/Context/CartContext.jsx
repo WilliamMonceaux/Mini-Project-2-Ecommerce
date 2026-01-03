4c57de48a49c0327ba4c1ada5a71fe4636b39be8
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-03 08:40:33 -0600

    created a new context that allows user to add products to cart

---
+++ mart-wal-.com/src/Context/CartContext.jsx
@@ -0,0 +1,19 @@
+import {useState, useContext} from 'react';
+
+const CartContext = React.createContext();
+
+function AddItemsToCart(props) {
+    const [addCart, setAddCart] = useState([]);
+
+    const handleAddToCart = (product) => {
+        setAddCart([...addCart, product]);
+    }
+
+    return(
+        <CartContext.Provider value= {{addCart, handleAddToCart }}>
+            {props.children}
+        </CartContext.Provider>
+    );
+}
+
+export { AddItemsToCart, CartContext };
