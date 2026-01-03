e5e02f483d949b34934dcc530e389e6f01cdc3be
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-02 20:27:49 -0600

    updated signup component name

--- mart-wal-.com/src/App.jsx
+++ mart-wal-.com/src/App.jsx
@@ -3,7 +3,7 @@ import { Home } from './pages/Home/Home.jsx';
 import { Shop } from './pages/Shop/Shop.jsx';
 import { Cart } from './pages/Cart/Cart.jsx';
 import { About } from './pages/About/About.jsx';
-import { SignUp } from './pages/Sign-up/SignUp.jsx';
+import SignUp from './pages/Sign-up/SignUp.jsx';
 
 
 function App() {
