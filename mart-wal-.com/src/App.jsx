56e003c4dfe16018e925ddeef1744332ac100be5
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-03 14:42:23 -0600

    imported Box from mui to help footer stay at bottom of page; imported Footer

--- mart-wal-.com/src/App.jsx
+++ mart-wal-.com/src/App.jsx
@@ -1,22 +1,30 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { Home } from './pages/Home/Home.jsx';
-import { Shop } from './pages/Shop/Shop.jsx';
-import { Cart } from './pages/Cart/Cart.jsx';
-import { About } from './pages/About/About.jsx';
-import {AddItemsToCart } from './Context/CartContext.jsx';
-import SignUp from './pages/Sign-up/SignUp.jsx';
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Home } from "./pages/Home/Home.jsx";
+import { Shop } from "./pages/Shop/Shop.jsx";
+import { Cart } from "./pages/Cart/Cart.jsx";
+import { About } from "./pages/About/About.jsx";
+import { Footer } from "./components/Footer/Footer.jsx";
+import { AddItemsToCart } from "./Context/CartContext.jsx";
+import SignUp from "./pages/Sign-up/SignUp.jsx";
 
 function App() {
   return (
     <AddItemsToCart>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/Shop' element={<Shop />} />
-      <Route path='/Cart' element={<Cart />} />
-      <Route path='/Sign-up' element={<SignUp />} />
-      <Route path='/About' element={<About />} />
-    </Routes>
+      <Box
+        sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+      >
+        <Box sx={{ flexGrow: 1 }}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Shop" element={<Shop />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/Sign-up" element={<SignUp />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+      </Box>
+      <Footer />
+      </Box>
     </AddItemsToCart>
   );
 }
