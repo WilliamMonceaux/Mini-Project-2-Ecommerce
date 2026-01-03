57d16fc1f6066267b24bef3305e6ba9c1d2c37b0
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-03 09:35:55 -0600

    imported context to remove items from cart; imported Elements from MUI; copied card template from CardTemplate component

--- mart-wal-.com/src/pages/Cart/Cart.jsx
+++ mart-wal-.com/src/pages/Cart/Cart.jsx
@@ -1,10 +1,77 @@
 import { NavBar } from '../../components/NavBar/NavBar';
 import { Header } from '../../components/Header/Header';
+import { useContext } from 'react';
+import { CartContext } from '../../Context/CartContext.jsx';
+import { Box, Typography, Grid, Card, Button, CardMedia, CardContent, CardActions } from '@mui/material';
 
 function Cart() {
+
+    const { addCart, removeProduct } = useContext(CartContext);
+
     return(<>
     <Header />
     <NavBar />
+    <Box>
+  <Grid container spacing={3}>
+          {addCart.map((product) => {
+            return (
+              <Grid
+                item
+                key={product.id}
+                size={{ xs: 12, md: 6, lg: 3 }}
+                display="flex"
+                justifyContent="center"
+              >
+                <Card
+                  sx={{
+                    width: 300,
+                    maxWidth: "100%",
+                    border: 2,
+                    boxShadow: 4,
+                    padding: 1,
+                    marginTop: '20px',
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    alt={product.title}
+                    sx={{ height: 200, objectFit: "contain" }}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {product.title}
+                    </Typography>
+                    <Typography
+                      variant="h5"
+                      sx={{ color: "success.main", mt: 1 }}
+                    >
+                      ${product.price.toFixed(2)}
+                    </Typography>
+                    <Typography variant='h6' component='div'>
+                      {product.description}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                    variant='contained'
+                      size="medium"
+                      sx={{ bgcolor: "#CC0000", color: "white" }}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        removeProduct(product.id);
+                        console.log(addCart);
+                      }}
+                    >
+                      Remove Item
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+    </Box>
     </>);
 }
 
