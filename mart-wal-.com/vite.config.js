c57918278c5d5477135c29cc634242ee8dc9c942
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-04 16:03:19 -0600

    fixed issue where store was hard coded to load store without Express server; refactored with prettier

--- mart-wal-.com/vite.config.js
+++ mart-wal-.com/vite.config.js
@@ -1,23 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://fakestoreapi.com',
+      "/api": {
+        target: "http://localhost:3000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/shop/, ''),
       },
     },
-    loglevel: 'debug',
+    loglevel: "debug",
   },
-})
+});
