ddf7de935292a52620cfcbd266cc15e4af2a0154
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-04 17:48:54 -0600

    imported PromoContainer and FeaturedProduct to showcase products on display; imported axios, useState, useEffect, and mui material; implemented useEffect to call product api; implemented product cards to display a few products that the ecommerce offers

--- mart-wal-.com/src/pages/Home/Home.jsx
+++ mart-wal-.com/src/pages/Home/Home.jsx
@@ -1,11 +1,76 @@
-import { PromoContainer } from '../../components/PromoContainer/PromoContainer';
+import { PromoContainer } from "../../components/PromoContainer/PromoContainer";
+
+import { FeaturedProduct } from "../../components/Cards/FeaturedProduct";
+
+import { Box, Typography, Grid } from "@mui/material";
+
+import { useEffect, useState } from "react";
+
+import axios from "axios";
 
 function Home() {
-    return(
-        <>
-        <PromoContainer />
-        </>
-    );
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get("/api/shop/products");
+        setProducts(response.data);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  const showCaseProducts = products.slice(0, 4);
+
+  return (
+    <PromoContainer>
+
+         <Typography
+          variant="h4"
+          gutterBottom
+          align="center"
+          sx={{ mb: 4, fontWeight: "Bold", fontSize: "2.6rem" }}
+        >
+          Quality finds, Cheaper prices
+        </Typography>
+        <Typography variant='h5' component='p' align='center' sx={{ 
+      mb: 6, 
+      maxWidth: '700px', 
+      mx: 'auto',
+      fontWeight: 400,
+      lineHeight: 1.6
+    }} >
+            Here you can find an assortment of products ranging from Men's Clothing, Jewerly, Electronics, and Women's Clothing. Start Shopping now!
+        </Typography>
+        
+<Box sx={{ p: 4, width: '100%' }}>
+
+  <Grid 
+    container 
+    spacing={4} 
+    justifyContent="center" 
+    alignItems="stretch"
+  >
+    {showCaseProducts.map((item) => (
+      <Grid 
+        item 
+        key={item.id} 
+        xs={12}    
+        md={6}    
+        lg={3}   
+        display="flex" 
+        justifyContent="center" 
+      >
+        <FeaturedProduct product={item} />
+      </Grid>
+    ))}
+  </Grid>
+</Box>
+    </PromoContainer>
+  );
 }
 
 export { Home };
