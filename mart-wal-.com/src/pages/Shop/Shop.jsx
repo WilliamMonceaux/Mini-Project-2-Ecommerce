2642f9f7be94bbdd8618d2d673eed74818766128
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-04 16:37:25 -0600

    added searchFilter and PrmoContainer components to make clear what each component is acheiving/doing

--- mart-wal-.com/src/pages/Shop/Shop.jsx
+++ mart-wal-.com/src/pages/Shop/Shop.jsx
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { CardsTemplate } from "../../components/Cards/CardsTemplate.jsx";
+import { PromoContainer } from "../../components/PromoContainer/PromoContainer.jsx";
+import { FilterBar } from "../../components/FilterBar/FilterBar.jsx";
 
 function Shop() {
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -19,9 +22,10 @@ function Shop() {
   }, []);
 
   return (
-    <>
+    <PromoContainer>
+      <FilterBar products={products} onFilterChange={setFilteredProducts} />
       <CardsTemplate products={products} />
-    </>
+    </PromoContainer>
   );
 }
 
