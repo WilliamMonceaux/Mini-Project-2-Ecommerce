25ad04c94eb71d1ca4fbc7a1a6aebfd9fdc8fcbc
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-03 19:31:21 -0600

    imported NavBar and Header component and rendered through here instead of inefficnetly importing and adding component to each page

--- mart-wal-.com/src/App.jsx
+++ mart-wal-.com/src/App.jsx
@@ -4,7 +4,10 @@ import { Home } from "./pages/Home/Home.jsx";
 import { Shop } from "./pages/Shop/Shop.jsx";
 import { Cart } from "./pages/Cart/Cart.jsx";
 import { About } from "./pages/About/About.jsx";
+import { NavBar } from './components/NavBar/NavBar.jsx';
+import { Header } from './components/Header/Header.jsx';
 import { Footer } from "./components/Footer/Footer.jsx";
+import { LogoutBtn } from './components/Logout/LogoutBtn.jsx';
 import { AddItemsToCart } from "./Context/CartContext.jsx";
 import { UserProvider } from './Context/UserContext.jsx';
 import SignUp from "./pages/Sign-up/SignUp.jsx";
@@ -13,6 +16,10 @@ function App() {
   return (
     <AddItemsToCart>
       <UserProvider>
+        <Header />
+        <NavBar />
+        <LogoutBtn />
+        
       <Box
         sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
       >
