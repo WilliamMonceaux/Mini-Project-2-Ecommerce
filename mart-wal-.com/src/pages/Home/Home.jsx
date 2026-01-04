c102c468fe272022ac5db0c20ae9dc057f15bdfa
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-03 19:42:36 -0600

    refactored with prettier

--- mart-wal-.com/src/pages/Home/Home.jsx
+++ mart-wal-.com/src/pages/Home/Home.jsx
@@ -1,12 +1,8 @@
-import { NavBar } from '../../components/NavBar/NavBar';
-import { Header } from '../../components/Header/Header';
 import { PromoContainer } from '../../components/PromoContainer/PromoContainer';
 
 function Home() {
     return(
         <>
-        <Header />
-        <NavBar />
         <PromoContainer />
         </>
     );
