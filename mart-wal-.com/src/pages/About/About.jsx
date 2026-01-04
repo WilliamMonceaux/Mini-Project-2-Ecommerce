90c34b1bfebe76e682ab16b20dddcc944ac55c8b
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-03 19:41:01 -0600

    refactored with prettier

--- mart-wal-.com/src/pages/About/About.jsx
+++ mart-wal-.com/src/pages/About/About.jsx
@@ -1,12 +1,8 @@
-import { NavBar } from '../../components/NavBar/NavBar';
-import { Header } from '../../components/Header/Header';
 import './about.css';
 
 
 function About() {
     return(<>
-    <Header />
-    <NavBar />
     <main>
         <section>
             <p>This project is to demonstrate my ability to apply using and creating React Components, using hooks, fetching and displaying data from external API, </p>
