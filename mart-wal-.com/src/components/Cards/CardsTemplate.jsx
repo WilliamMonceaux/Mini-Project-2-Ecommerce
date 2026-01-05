e4d500db59030d41e56c1b7eed44792f60ea428f
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-05 08:03:39 -0600

    added more semanticness and refactored with prettier

--- mart-wal-.com/src/components/Cards/CardsTemplate.jsx
+++ mart-wal-.com/src/components/Cards/CardsTemplate.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -6,77 +6,79 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import { CartContext } from '../../Context/CartContext.jsx';
+import Box from "@mui/material/Box";
+import { CartContext } from "../../Context/CartContext.jsx";
 
 function CardsTemplate({ products }) {
-
   const [displayProducts, setDisplayProducts] = useState(products);
 
-    const { handleAddToCart } = useContext(CartContext);
+  const { handleAddToCart } = useContext(CartContext);
 
   useEffect(() => {
     setDisplayProducts(products);
   }, [products]);
 
   return (
-    <main className="card-container">
-        <Grid container spacing={3}>
-          {displayProducts.map((product) => {
-            return (
-              <Grid
-                item
-                key={product.id}
-                size={{ xs: 12, md: 6, lg: 3 }}
-                display="flex"
-                justifyContent="center"
+    <Box component="main">
+      <Grid component="ul" container spacing={3}>
+        {displayProducts.map((product) => {
+          return (
+            <Grid
+              item
+              key={product.id}
+              size={{ xs: 12, md: 6, lg: 3 }}
+              display="flex"
+              justifyContent="center"
+            >
+              <Card
+                component="article"
+                sx={{
+                  width: 300,
+                  maxWidth: "100%",
+                  border: 2,
+                  boxShadow: 4,
+                  padding: 1,
+                }}
               >
-                <Card
-                  sx={{
-                    width: 300,
-                    maxWidth: "100%",
-                    border: 2,
-                    boxShadow: 4,
-                    padding: 1,
-                  }}
-                >
-                  <CardMedia
-                    component="img"
-                    image={product.image}
-                    alt={product.title}
-                    sx={{ height: 200, objectFit: "contain" }}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {product.title}
-                    </Typography>
-                    <Typography
-                      variant="h5"
-                      sx={{ color: "success.main", mt: 1 }}
-                    >
-                      ${product.price.toFixed(2)}
-                    </Typography>
-                    <Typography variant='body1' component='div'>
-                      {product.description}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button
-                      size="medium"
-                      sx={{ bgcolor: "black", color: "white" }}
-                      onClick={(e) => {
-                        e.preventDefault();
-                        handleAddToCart(product);
-                      }}
-                    >
-                      Add to Card
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            );
-          })}
-        </Grid>
-    </main>
+                <CardMedia
+                  component="img"
+                  image={product.image}
+                  alt={product.title}
+                  sx={{ height: 200, objectFit: "contain" }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {product.title}
+                  </Typography>
+                  <Typography
+                    variant="h6"
+                    component="p"
+                    sx={{ color: "success.main", mt: 1 }}
+                  >
+                    ${product.price.toFixed(2)}
+                  </Typography>
+                  <Typography variant="body1" component="p">
+                    {product.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    size="medium"
+                    sx={{ bgcolor: "black", color: "white" }}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleAddToCart(product);
+                    }}
+                  >
+                    Add to Card
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Box>
   );
 }
 
