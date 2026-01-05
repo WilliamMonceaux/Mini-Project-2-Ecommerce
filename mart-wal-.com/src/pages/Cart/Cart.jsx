41dc6e7c2efc7b15fadf447bcb1e9fd7f0ddf3d5
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-05 08:03:12 -0600

    added more semanticness and refactored with prettier

--- mart-wal-.com/src/pages/Cart/Cart.jsx
+++ mart-wal-.com/src/pages/Cart/Cart.jsx
@@ -15,69 +15,69 @@ function Cart() {
   const { addCart, removeProduct } = useContext(CartContext);
 
   return (
-      <Box component='main'>
-        <Grid container spacing={3}>
-          {addCart.map((product) => {
-            return (
-              <Grid
-                item
-                key={product.id}
-                size={{ xs: 12, md: 6, lg: 3 }}
-                display="flex"
-                justifyContent="center"
-                component='ul'
+    <Box component="main">
+      <Grid container spacing={3}>
+        {addCart.map((product) => {
+          return (
+            <Grid
+              item
+              key={product.id}
+              size={{ xs: 12, md: 6, lg: 3 }}
+              display="flex"
+              justifyContent="center"
+              component="ul"
+            >
+              <Card
+                component="article"
+                sx={{
+                  width: 300,
+                  maxWidth: "100%",
+                  border: 2,
+                  boxShadow: 4,
+                  padding: 1,
+                  marginTop: "20px",
+                }}
               >
-                <Card
-                component='article'
-                  sx={{
-                    width: 300,
-                    maxWidth: "100%",
-                    border: 2,
-                    boxShadow: 4,
-                    padding: 1,
-                    marginTop: "20px",
-                  }}
-                >
-                  <CardMedia
-                    component="img"
-                    image={product.image}
-                    alt={product.title}
-                    sx={{ height: 200, objectFit: "contain" }}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {product.title}
-                    </Typography>
-                    <Typography
-                      variant="h5"
-                      sx={{ color: "success.main", mt: 1 }}
-                    >
-                      ${product.price.toFixed(2)}
-                    </Typography>
-                    <Typography variant="body1" component="div">
-                      {product.description}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button
-                      variant="contained"
-                      size="medium"
-                      sx={{ bgcolor: "#CC0000", color: "white" }}
-                      onClick={(e) => {
-                        e.preventDefault();
-                        removeProduct(product.id);
-                        console.log(addCart);
-                      }}
-                    >
-                      Remove Item
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            );
-          })}
-        </Grid>
-      </Box>
+                <CardMedia
+                  component="img"
+                  image={product.image}
+                  alt={product.title}
+                  sx={{ height: 200, objectFit: "contain" }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {product.title}
+                  </Typography>
+                  <Typography
+                    variant="h5"
+                    sx={{ color: "success.main", mt: 1 }}
+                  >
+                    ${product.price.toFixed(2)}
+                  </Typography>
+                  <Typography variant="body1" component="div">
+                    {product.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    variant="contained"
+                    size="medium"
+                    sx={{ bgcolor: "#CC0000", color: "white" }}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      removeProduct(product.id);
+                      console.log(addCart);
+                    }}
+                  >
+                    Remove Item
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Box>
   );
 }
 
