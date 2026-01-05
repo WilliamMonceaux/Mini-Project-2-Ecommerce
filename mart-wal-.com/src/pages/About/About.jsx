ea3869be64c92752d45c31fc6ca3d74463bc12d7
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-05 07:48:40 -0600

    imported mui material and refactored with  mui grid styling instead of  regular css

--- mart-wal-.com/src/pages/About/About.jsx
+++ mart-wal-.com/src/pages/About/About.jsx
@@ -1,22 +1,28 @@
-import "./about.css";
+import { Grid, Typography, Container, Box } from "@mui/material";
 
 function About() {
   return (
-    <>
-      <main>
-        <section>
-          <p>
-            This project is to demonstrate my ability to apply using and
-            creating React Components, using hooks, fetching and displaying data
-            from external API,{" "}
-          </p>
-          <p>
-            integrating React UI Library (This case Material UI), and with Git
-            commits and pushes.
-          </p>
-        </section>
-      </main>
-    </>
+    <main>
+      <Box component="main">
+        <Container maxWidth="lg" sx={{ mt: 8 }}>
+          <Grid component="section" container justifyContent="center">
+            <Grid item sx={{ xs: 12, md: 10 }}>
+              <Typography
+                variant="h3"
+                component="body1"
+                align="center"
+                sx={{ lineHeight: 2 }}
+              >
+                This project is to demonstrate my ability to apply using and
+                creating React Components, using hooks, fetching and displaying
+                data from external API, integrating React UI Library (This case
+                Material UI), and with Git commits and pushes.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </main>
   );
 }
 
