d7d97eb691fadb3ac16d85e637882aa33608d613
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-08 17:49:11 -0600

    readjusted Footer font size for easier readability

--- mart-wal-.com/src/components/Footer.jsx
+++ mart-wal-.com/src/components/Footer.jsx
@@ -8,13 +8,12 @@ function Footer() {
         py: 2, 
         px: 2, 
         mt: 'auto',
-        fontSize: '1.2rem',
         backgroundColor: 'black', 
         color: 'white' 
       }}
     >
       <Container maxWidth="lg">
-        <Typography variant="body1" align="center">
+        <Typography variant="body1" align="center" sx={{ fontSize: '1.4rem'}}>
           © 2026 Mart-Wal. All rights reserved.
         </Typography>
       </Container>
