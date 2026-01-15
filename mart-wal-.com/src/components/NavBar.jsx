7ff58d8aae575de0d815407dca44dc7109779246
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-15 07:32:03 -0600

    replaced regular html/css navbar for MUI AppBar with hamburger menu for cleaner look  and more responsiveness

--- mart-wal-.com/src/components/NavBar.jsx
+++ mart-wal-.com/src/components/NavBar.jsx
@@ -1,32 +1,132 @@
 import { NavLink } from "react-router-dom";
 import { useUserContext } from "../Context/UserContext";
 import "../index.css";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+
+const pages = [
+  { Title: "Home", Path: "/" },
+  { Title: "Shop", Path: "/Shop" },
+  { Title: "Cart", Path: "/Cart" },
+  { Title: "About", Path: "/About" },
+  { Title: "SignUp", Path: "/Signup" },
+];
 
 function NavBar() {
   const { currentUser } = useUserContext();
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
-    <nav className="navbar component-box">
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/shop">Shop</NavLink>
-        </li>
-        <li>
-          <NavLink to="/cart">Cart</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-        {!currentUser?.name && (
-          <li>
-            <NavLink to="/sign-up">Signup</NavLink>
-          </li>
-        )}
-      </ul>
-    </nav>
+    <AppBar position="static" sx={{ backgroundColor: "#212121" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon sx={{ fontSize: "3rem" }} />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: "block", md: "none" } }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.Title} onClick={handleCloseNavMenu}>
+                  <NavLink to={page.Path} style={{ textDecoration: 'none', color: 'inherit'}}>
+                    <Typography
+                      sx={{ textAlign: "center", fontSize: "1.6rem" }}
+                    >
+                      {page.Title}
+                    </Typography>
+                  </NavLink>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex", justifyContent: "flex-end" },
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page.Title}
+                component={NavLink}
+                to={page.Path}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  color: "white",
+                  display: "block",
+                  fontSize: "1.6rem",
+                }}
+              >
+                {page.Title}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 
