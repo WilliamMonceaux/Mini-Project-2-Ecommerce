8a6be9176c86bb11f2739ca2efb06da22332a8cd
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-17 15:16:07 -0600

    removed FeaturedProduct component and used Products component to display products instead; removed Grid MUI material; made text align for product cards

--- mart-wal-.com/src/pages/Home.jsx
+++ mart-wal-.com/src/pages/Home.jsx
@@ -1,67 +1,51 @@
-import { FeaturedProduct } from "../components/FeaturedProduct";
-import { Products } from '../components/Products';
-import { Box, Typography, Grid } from "@mui/material";
-import { useFetchContext } from '../Context/FetchContext';
+import { Products } from "../components/Products";
+import { Box, Typography } from "@mui/material";
+import { useFetchContext } from "../Context/FetchContext";
 
 function Home() {
   const { products } = useFetchContext();
-
-  const showCaseProducts = products.slice(0, 4);
+  const featuredProducts = products.slice(0, 4);
 
   return (
     <Box component="main">
-        <Typography
-          variant="h3"
-          component="h2"
-          gutterBottom
-          align="center"
-          sx={{ my: 4, fontWeight: "Bold" }}
-        >
-          Quality finds, Cheaper prices
-        </Typography>
-        <Typography
-          variant="h4"
-          component="p"
-          align="center"
-          sx={{
-            mb: 6,
-            maxWidth: "700px",
-            fontFamily: 'Roboto',
-            mx: "auto",
-            fontWeight: 400,
-            lineHeight: 1.6,
-          }}
-        >
-          Here you can find an assortment of products ranging from Men's
-          Clothing, Jewerly, Electronics, and Women's Clothing.
-        </Typography>
+      <Typography
+        variant="h3"
+        component="h2"
+        gutterBottom
+        align="center"
+        sx={{ my: 4, fontWeight: "Bold" }}
+      >
+        Quality finds, Cheaper prices
+      </Typography>
+      <Typography
+        variant="h4"
+        component="p"
+        align="center"
+        sx={{
+          mb: 6,
+          maxWidth: "700px",
+          fontFamily: "Roboto",
+          mx: "auto",
+          fontWeight: 400,
+          lineHeight: 1.6,
+        }}
+      >
+        Here you can find an assortment of products ranging from Men's Clothing,
+        Jewerly, Electronics, and Women's Clothing.
+      </Typography>
 
-        <Typography variant='h3' component='h3' gutterBottom sx={{ fontWeight: 'Bold', textAlign: 'center', mt: '8rem' }}>
-          Featured Products
-        </Typography>
+      <Typography
+        variant="h3"
+        component="h3"
+        gutterBottom
+        sx={{ fontWeight: "Bold", textAlign: "center", mt: "8rem" }}
+      >
+        Featured Products
+      </Typography>
 
-        <Box component="section" sx={{ p: 4, width: "100%" }}>
-          <Grid
-            container
-            spacing={4}
-            justifyContent="center"
-            alignItems="stretch"
-          >
-            {showCaseProducts.map((item) => (
-              <Grid
-                item
-                key={item.id}
-                xs={12}
-                md={6}
-                lg={3}
-                display="flex"
-                justifyContent="center"
-              >
-                <FeaturedProduct product={item} />
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
+      <Box component="section" sx={{ p: 4, width: "100%" }}>
+        <Products products={featuredProducts} />
+      </Box>
     </Box>
   );
 }
