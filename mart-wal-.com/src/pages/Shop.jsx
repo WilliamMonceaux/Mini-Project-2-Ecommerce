721baf02f2841b23b5dd87080777f300f1ecf6ca
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-17 14:40:57 -0600

    imported Button and CartContext; implementing add to cart button logic here to let the CardTemplate component be  more dynamic for other uses

--- mart-wal-.com/src/pages/Shop.jsx
+++ mart-wal-.com/src/pages/Shop.jsx
@@ -4,11 +4,14 @@ import { PromoContainer } from "../components/PromoContainer.jsx";
 import { FilterBar } from "../components/FilterBar.jsx";
 import { Typography } from "@mui/material";
 import { Box } from "@mui/material";
+import { Button } from "@mui/material";
 import { useFetchContext } from "../Context/FetchContext.jsx";
+import { useCartContext } from '../Context/CartContext.jsx';
 
 function Shop() {
   const [filteredProducts, setFilteredProducts] = useState([]);
   const { products } = useFetchContext();
+  const { addToCart } = useCartContext();
 
   return (
     <Box component="main">
@@ -17,7 +20,20 @@ function Shop() {
           Shop for Products:
         </Typography>
         <FilterBar products={products} onFilterChange={setFilteredProducts} />
-        <CardsTemplate products={filteredProducts} />
+        <CardsTemplate products={filteredProducts}>
+           {(product) => (
+             <Button
+                    size="medium"
+                    sx={{ bgcolor: "black", color: "white", fontWeight: 800 }}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      addToCart(product);
+                    }}
+                  >
+                    Add to Cart
+                  </Button> 
+          )}
+        </CardsTemplate>
       </PromoContainer>
     </Box>
   );
