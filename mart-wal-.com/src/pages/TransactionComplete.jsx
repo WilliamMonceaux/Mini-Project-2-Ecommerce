1b69fe67630557afd236a882d8323dfc1a96e900
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-01-26 17:33:22 -0600

    added the draft of Transaction complete text to display when you purchase items

---
+++ mart-wal-.com/src/pages/TransactionComplete.jsx
@@ -0,0 +1,13 @@
+
+function TransactionComplete() {
+    return(
+        <main>
+            <div>
+            <h1> Thank you for your order</h1>
+            <p> Your transaction is now complete! Your items will be shipped at *insert dynamic address* </p>
+            </div>
+        </main>
+    );
+}
+
+export { TransactionComplete };
