bce0d8ad70c9c4f235c916f3c0efb673209f496a
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-05-16 11:32:18 -0500

    feat(models): mplement Product, User, and OrderItem data schemas Created Product schema with price decimal precision and string length adjustments. Implemented User model with strict email validations and automatic lowercase data normalization. Added OrderItem as a transactional join table to handle itemized order lines. Established comprehensive column constraints and foreign key references across all three files.

---
+++ models/product.js
@@ -0,0 +1,41 @@
+const { DataTypes, Model } = require("sequelize");
+let { sequelize } = require("../config/dbConnect");
+
+class Product extends Model {}
+
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING(1000),
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "product",
+    timestamps: true,
+    freezeTableName: true,
+  },
+);
