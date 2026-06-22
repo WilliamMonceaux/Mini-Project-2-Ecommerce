b574bbdb5b2e936f8a9aa92c19e7d0f93e1b7a5a
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-06-22 07:49:02 -0500

    feat(controller): implement addItemToCart to support product additions

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -21,6 +21,27 @@ async function getCartItems(req, res) {
   }
 }
 
+
+/**
+ * @description Add products into the cart
+ * @route api/cart/add
+*/
+
+async function addItemToCart(req, res) {
+  try {
+   const newProductInCart = await Cart.create(req.body);
+
+   return res
+   .status(201)
+   .json({ success: true, data: newProductInCart});
+    
+  } catch (err) {
+    console.error('Could not add product into cart', err.message);
+    res.status(500).json({ success: false, data: null, error: err.message});
+  }
+}
+
 module.exports = {
   getCartItems,
+  addItemToCart,
 };
