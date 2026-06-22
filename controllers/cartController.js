89ef1c60b7d3a3da99f95daaf8581699ad7ef384
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-06-22 07:25:12 -0500

    feat(controller): implement getCartItems controller for cart management Create getCartItems function to retrieve all cart records from the database Export controller for use in cart routes

---
+++ controllers/cartController.js
@@ -0,0 +1,26 @@
+"use strict";
+const { Cart, Product } = require("../models");
+
+/**
+ * @description Fetches all products in cart
+ * @route api/cart
+ */
+
+async function getCartItems(req, res) {
+  try {
+    const productsInCart = await Cart.findAll({});
+
+    console.log("Received all products in cart.");
+
+    return res.status(200).json({ success: true, data: productsInCart });
+  } catch (err) {
+    console.error("Could not receive products in cart.", err.message);
+    return res
+      .status(500)
+      .json({ success: false, data: null, error: err.message });
+  }
+}
+
+module.exports = {
+  getCartItems,
+};
