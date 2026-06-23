414b379d2efeca3429f6c8cb3acfd4917a04ab69
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-06-23 16:41:29 -0500

    feat(routes): register user routes in app.js Add userRoutes to application middleware Mount user routes at /api/user Standardize formatting for existing route declarations

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const productRoutes = require('./routes/productRoute');
-const cartRoutes = require('./routes/cartRoute');
+const express = require("express");
+const productRoutes = require("./routes/productRoute");
+const cartRoutes = require("./routes/cartRoute");
+const userRoutes = require("./routes/userRoute");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Mini-Project 2 Backend Server')
+app.get("/", (req, res) => {
+  res.send("Mini-Project 2 Backend Server");
 });
 
-app.use('/api/cart', cartRoutes );
+app.use("/api/cart", cartRoutes);
 
-app.use('/api/shop', productRoutes );
+app.use("/api/shop", productRoutes);
+
+app.use("/api/user", userRoutes);
 
 module.exports = { app };
