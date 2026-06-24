52f4c4e1796b099e5e6286c58a85097a9025942b
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-06-24 07:56:24 -0500

    feat(routes): add GET route to fetch all users Expose GET /api/user endpoint Connect getUsers controller to retrieve user data

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { createUser } = require("../controllers/userController");
+const { createUser, getUsers } = require("../controllers/userController");
+
+router.get('/', getUsers);
 
 router.post("/add", createUser);
 
