c7303b7e84fb6f90792c8f2ce65ee03ada4089f5
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-07-01 08:11:57 -0500

    refactor(routes): remove orderItemRoute Delete unused orderItemRoute.js Remove endpoint following the centralization of creation logic into the checkout transaction

--- routes/orderItemRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createOrderItem } = require("../controllers/orderItemController");
-
-router.post("/", createOrderItem);
-
-module.exports = router;
